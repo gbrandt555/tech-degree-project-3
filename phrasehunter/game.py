1bf78e6ca444675f8aa864dc01fc8f9dd99e78fb
gbrandt555 <georgebrandt7841@gmail.com>
2020-09-08 12:43:03 -0500

    initial commit

---
+++ phrasehunter/game.py
@@ -0,0 +1,37 @@
+class Game:
+
+    def __init__(self, missed, phrases, active_phrase, guesses):
+        missed = 0
+        phrases = []
+        active_phrase = None
+        guesses = []
+    
+
+    def start():
+        #calls welcome method
+        welcome()
+        #create game loop
+        #calls get_guesses()
+        #add users guesses to guesses
+        #increment missed on incorrect guess
+        #call game_over()
+    
+
+    def get_random_phrase():
+        pass
+        #randomly selects a phease stored in phrases list and return it
+    
+
+    def welcome():
+        #display welcome message to the player
+        print("Hello player")
+    
+
+    def get_guess():
+        pass
+        #gets the guess from the user and records it in guesses
+    
+
+    def game_over():
+        pass
+        #display win or loss message
---
+++ phrasehunter/phrase.py
@@ -0,0 +1,17 @@
+class Phrase:
+
+    def __init__(self, phrase):
+        self.phrase = phrase
+        phrase = Phrase
+    
+
+    def display():
+        #print out phrase with '_' as placeholder characters
+
+    def check_letter():
+        #check to see if letter selected matches letter in phrase
+    
+
+    def check_complete():
+        #check to see if whole phrase has been guessed
+
